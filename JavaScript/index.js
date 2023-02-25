82e1f0e37f30be988201f810637493fccec6f079
Safoun <safoun.official@gmail.com>
2023-02-25 20:53:32 +0600

    made a database type website containig every single counry info using api and it's also responsive :)

---
+++ JavaScript/index.js
@@ -0,0 +1,33 @@
+const asia = "asia";
+const africa = "africa";
+const europe = "europe";
+const america = "americas";
+const oceania = "oceania";
+
+function getAPI(region) {
+    fetch(`https://restcountries.com/v3.1/region/${region}`)
+        .then(data => data.json())
+        .then(json => processData(json))
+        .catch(error => console.log(error))
+}
+
+const processData = data => {
+    const countryContainer = document.getElementById("country-container")
+    data.forEach(element => {
+        console.log(element);
+        const country = document.createElement("div");
+        country.classList.add("fs-6", "card", "col-10", "col-md-6", "col-lg-4", "mx-auto", "custom-card", "p-0")
+        const currencies = Object.keys(element.currencies).join(", ");
+        country.innerHTML = `
+            <img class="img-fluid h-100 rounded-top-2" src=${element.flags.png}>
+            <div class="card-body">
+                <div>Country Name : ${element.name.common}</div>
+                <div>Capital : ${element.capital[0]}</div>
+                <div>Currency : ${currencies}</div>
+            </div>
+        `
+        countryContainer.appendChild(country);
+    });
+}
+
+getAPI(asia);
